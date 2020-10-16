1957705139ce96d912b9049aac7bec933417d4cb
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2020-10-16 19:18:45 -0400

    updated file structure -- create react and php backend

--- database_info/table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO
-  destination ( location, img, rating, cost, title, dated, description )
-VALUES
-  ( 'California' , 'www.google.com' , 4, 123, 'test', '2016-06-22', 'testing' ); -- create a row
